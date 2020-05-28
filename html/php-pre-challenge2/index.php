9a182781e48ff67d3f7f754573b6e2157ea6def7
Asako Oda <12odadao14@gmail.com>
2020-05-28 15:36:06 +0900

    update ループの回転数の削減

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,8 +3,8 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 $length = count($array);
-for ($i = 0; $i < $length; $i++) {
-    for ($j = 0; $j < $length - 1; $j++) {
+for ($i = 0; $i < $length -1; $i++) {
+    for ($j = 0; $j < $length - 1 -$i; $j++) {
         if ($array[$j] > $array[$j + 1]) {
             $temp = $array[$j];
             $array[$j] = $array[$j + 1];
