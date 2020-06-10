7f1f2b20d6c29dd1c6e3f3dbdfffe4cc22485af6
Asako Oda <12odadao14@gmail.com>
2020-06-10 16:29:31 +0900

    add リツイート機能、いいね機能

--- html/php-challenge/index.php
+++ html/php-challenge/index.php
@@ -82,11 +82,77 @@ if ($_GET['like'] === 'delete') {
 	header('Location: index.php');
 	exit;
 }
-//投稿ごとのいいね数を取得するlikesテーブルの各like_post_idをカウントしたらいけると思ったんだけどな=>いけた！！✌︎('ω'✌︎ )
+//投稿ごとのいいね数を取得するにはlikesテーブルの各like_post_idをカウントしたらいけると思ったんだけどな=>いけた！！✌︎('ω'✌︎ )
 //$post['id']の値が必要なので、foreach文内でSQLを実行した
 $numsOfLike = $db->prepare('SELECT COUNT(id) AS cntLike FROM likes WHERE like_post_id=?');
 
 //いいね機能終わり
+//以下リツイート機能
+
+//リツイートする
+if ($_POST['retweet'] === 'send') {
+	//ある投稿を再投稿する
+	$reposts = $db->prepare('INSERT INTO posts SET message=?, member_id=?, retwi_member_id=?, retwi_origin_id=?, created=NOW()');
+	$reposts->execute(array(
+		$_POST['origin_message'],
+		$_POST['origin_member_id'],
+		$member['id'],
+		$_POST['origin_id']
+	));
+	//リツイート情報を登録する
+	$recordReposts = $db->prepare('INSERT INTO reposts SET my_member_id=?, origin_member_id=?, origin_id=?, created=NOW()');
+	$recordReposts->execute(array(
+		$member['id'],
+		$_POST['origin_member_id'],
+		$_POST['origin_id']
+	));
+	//リツイート元の投稿にいいねがついていた場合どうする？？
+	//like_post_id=reposts.origin_id
+	header('Location: index.php');
+	exit();
+}
+//リツイートを取り消す
+if ($_POST['retweet'] === 'delete') {
+	//まず消したいリツイートを特定する。
+	//一つのアカウントが一つの投稿をリツイートできるのは１回限りなので、そこから特定
+	//repostsテーブルからの削除
+	$getRetweetId = $db->prepare('SELECT id FROM reposts WHERE my_member_id=? AND origin_id=?');
+	$getRetweetId->execute(array(
+		$member['id'],
+		$_POST['origin_id']
+	));
+	$retweetId = $getRetweetId->fetch();
+	$deleteRetweetInfo = $db->prepare('DELETE FROM reposts WHERE id=?');
+	$deleteRetweetInfo->execute(array($retweetId['id']));
+
+	//postsテーブルからの削除
+	//post.idの特定
+	$getPostsId = $db->prepare('SELECT id FROM posts WHERE retwi_member_id=? AND retwi_origin_id=?');
+	$getPostsId->execute(array(
+		$member['id'],
+		$_POST['origin_id']
+	));
+	$postId = $getPostsId->fetch();
+	$deleteRepost = $db->prepare('DELETE FROM posts WHERE id=?');
+	$deleteRepost->execute(array($postId['id']));
+	header('Location: index.php');
+	exit;
+}
+//ログインしている人が以前行ったリツイートを取得する
+//一つのアカウントが一つの投稿をリツイートできるのは１回限り。
+//repostsテーブルからログインしている人の、リツイートしたorigin_idを取得する
+$getRetweets = $db->prepare('SELECT origin_id FROM reposts WHERE my_member_id=? ORDER BY origin_id ASC');
+$getRetweets->execute(array($member['id']));
+while ($getRetweet = $getRetweets->fetchColumn()) {
+	$retweetArr[] = (int) $getRetweet;
+};
+
+//一つの投稿に関して、リツイートされた回数を調べるため、プリペアドステートメントを準備する
+$cntRetweets = $db->prepare('SELECT COUNT(id) AS cntRetweet FROM reposts WHERE origin_id=?');
+
+//リツイートした人の情報を取得するためのプリペアドステートメントを準備する
+$retMember = $db->prepare('SELECT m.name FROM members m, posts p WHERE m.id=p.retwi_member_id AND p.id=?');
+
 
 // 返信の場合
 if (isset($_REQUEST['res'])) {
@@ -108,7 +174,7 @@ function makeLink($value)
 {
 	return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>', $value);
 }
-//バイナリサーチ（post['id']に対していいねをしたかどうか、true/falseで返すため）
+//バイナリサーチ（ログインした人が以前いいね/RTをしたリストの中に、post['id']が含まれているかどうか、true/falseで返す）
 function binarySearch($array, $target)
 {
 	$array = (array) $array;
@@ -166,43 +232,157 @@ function binarySearch($array, $target)
 				</div>
 			</form>
 
+			<!--ここから先の構造-->
+			<!--その投稿が、リツイートされたものであるか否かをまず判断する-->
+
+			<!--リツイートされたものである場合-->
+			
+			<!--いいね機能-->
+			<!--いいねをすると、元の投稿のいいね数に反映させたい=>likesテーブルのlike_post_idには、postsテーブルのretwi_origin_idを渡す✌︎('ω'✌︎ )-->
+			<!--リツイート後の投稿にも、いいね数を反映させたい=>処置は上記同様✌︎('ω'✌︎ )-->
+			<!--リツイート後の投稿からも、いいねを取り消せるようにしたい=>処置は上記同様✌︎('ω'✌︎ )-->
+			
+			<!--リツイート機能-->
+			<!--リツイートをすると、元の投稿のリツイート数に反映させたい=>repostsテーブルのorigin_idに、postsテーブルのretwi_origin_idを渡してみる✌︎('ω'✌︎ )-->
+			<!--リツイート後の投稿にも、リツイート数を反映させたい=>処置は上記同様✌︎('ω'✌︎ )-->
+			<!--リツイート後の投稿からも、リツイートを取り消したい=>処置は上記同様✌︎('ω'✌︎ )-->
+			<!--その際、再投稿されたpost自体も削除したい-->
+			<!--一つのアカウントが、一つの投稿に対してリツイートできるのは１回限り。そのため、postsテーブルのidを特定するには、postsテーブルにリツイートした人のカラムが必要-->
+			<!--retwi_member_idとretwi_origin_idでposts.idを特定して削除できるかも=>できた✌︎('ω'✌︎ )-->
+
+			<!--リツイートされたものではない場合-->
+			
+
 			<?php foreach ($posts as $post) : ?>
 				<div class="msg">
+					<!--その投稿がリツイートされたものだった場合、リツイートした人を表示する-->
+					<?php if ($post['retwi_origin_id'] > 0) {
+						$retMember->execute(array($post['id']));
+						$retMemberName = $retMember->fetch();
+						?>
+						<img src="images/inforetweet.png" alt="リツイートされました" width="12px" height="12px">
+						<p class="msg__retweet">Retweeted by <?php echo $retMemberName['name']; ?></p>
+					<?php } ?>
+					<!--表示終わり-->
 					<img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']); ?>" />
 					<p><?php echo makeLink(h($post['message'])); ?><span class="name">（<?php echo h($post['name']); ?>）</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
 					<div class="post_info">
 						<p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a></p>
 						<p class="like">
 							<?php
-							$numsOfLike->execute(array($post['id']));
-							$cntLike = $numsOfLike->fetchColumn();
-							if (!binarySearch($likePostArr, $post['id'])) : ?>
-								<!--いいね機能-->
-								<form action="" method="get">
-									<input type="hidden" name="like" value="add">
-									<input type="hidden" name="like_post_id" value="<?php echo $post['id']; ?>">
-									<input type="image" name="like" src="images/like.png" alt="いいね" width="13px" height="13px">
-									<?php if ($cntLike > 0) : ?>
-										<input type="submit" class="like_form" value="<?php echo h($cntLike); ?>">
-									<?php endif; ?>
-								</form>
-							<?php else : ?>
-								<!--いいね取り消し機能-->
-								<form action="" method="get">
-									<input type="hidden" name="like" value="delete">
-									<input type="hidden" name="like_post_id" value="<?php echo $post['id']; ?>">
-									<input type="image" src="images/after_like.png" alt="いいね取り消し" width="13px" height="13px">
-									<?php if ($cntLike > 0) : ?>
-										<input type="submit" class="like_form on_like_form" value="<?php echo h($cntLike); ?>">
-									<?php endif; ?>
-								</form>
-							<?php endif; ?>
-							<!--以下リツイート機能-->
-							<form action="" method="post">
-								<input type="hidden" name="retweet" value="send">
-								<input type="hidden" name="retwi_post_id" value="<?php echo h($post['id']);?>">
-								<input type="image" src="images/retweet.png" alt="リツイートする" width="13px" height="13px">
-							</form>
+							//もしその投稿がリツイートされたものだった場合
+
+							if ($post['retwi_origin_id'] > 0) { 
+								//likesテーブルにはposts.idではなくretwi_origin_idを渡す
+								$numsOfLike->execute(array($post['retwi_origin_id']));
+								$cntLike = $numsOfLike->fetchColumn();
+								//まずいいね機能から
+								if (!binarySearch($likePostArr, $post['retwi_origin_id'])) : ?>
+									<!--その投稿にいいねしたことがないならば？-->
+									<!--いいね機能-->
+									<form action="" method="get">
+										<input type="hidden" name="like" value="add">
+										<input type="hidden" name="like_post_id" value="<?php echo $post['retwi_origin_id']; ?>">
+										<input type="image" name="like" src="images/like.png" alt="いいね" width="13px" height="13px">
+										<?php if ($cntLike > 0) : ?>
+											<input type="submit" class="like_form" value="<?php echo h($cntLike); ?>">
+										<?php endif; ?>
+									</form>
+								<?php else : ?>
+									<!--いいね取り消し機能-->
+									<form action="" method="get">
+										<input type="hidden" name="like" value="delete">
+										<input type="hidden" name="like_post_id" value="<?php echo $post['retwi_origin_id']; ?>">
+										<input type="image" src="images/after_like.png" alt="いいね取り消し" width="13px" height="13px">
+										<?php if ($cntLike > 0) : ?>
+											<input type="submit" class="like_form on_like_form" value="<?php echo h($cntLike); ?>">
+										<?php endif; ?>
+									</form>
+								<?php endif; ?>
+								<!--以下リツイート機能-->
+								<?php
+								$cntRetweets->execute(array($post['retwi_origin_id']));
+								$cntRetweet = $cntRetweets->fetchColumn();
+								if (!binarySearch($retweetArr, $post['retwi_origin_id'])) : ?>
+									<!--投稿に対して、リツイートしたことがない場合-->
+									<form action="" method="post">
+										<input type="hidden" name="retweet" value="send">
+										<input type="hidden" name="origin_message" value="<?php echo ($post['message']); ?>">
+										<input type="hidden" name="origin_member_id" value="<?php echo ($post['member_id']); ?>">
+										<input type="hidden" name="origin_id" value="<?php echo ($post['retwi_origin_id']); ?>">
+										<input type="image" src="images/retweet.png" alt="リツイートする" width="13px" height="13px">
+										<?php if ($cntRetweet > 0) : ?>
+											<input type="submit" class="like_form" value="<?php echo h($cntRetweet); ?>">
+										<?php endif; ?>
+									</form>
+								<?php else : ?>
+									<!--リツイートしたことがある＝取り消す-->
+									<form action="" method="post">
+										<input type="hidden" name="retweet" value="delete">
+										<input type="hidden" name="origin_id" value="<?php echo h($post['retwi_origin_id']); ?>">
+										<input type="hidden" name="afterRetwiId" value="<?php echo h($post['id']); ?>">
+										<input type="image" src="images/after_retweet.png" alt="リツイートを取り消す" width="13px" height="13px">
+										<?php if ($cntRetweet > 0) : ?>
+											<input type="submit" class="like_form on_retweet_form" value="<?php echo h($cntRetweet); ?>">
+										<?php endif; ?>
+									</form>
+								<?php endif; ?>
+
+								<?php
+								//その投稿が、リツイートされたものではない場合
+							} else {
+								$numsOfLike->execute(array($post['id'])); //いいね数をカウントするプリペアドステートメントの、likes.like_post_idにその投稿ごとのposts.idを渡している
+								$cntLike = $numsOfLike->fetchColumn();
+								if (!binarySearch($likePostArr, $post['id'])) : ?>
+									<!--その投稿にいいねしたことがないならば？-->
+									<!--いいね機能-->
+									<form action="" method="get">
+										<input type="hidden" name="like" value="add">
+										<input type="hidden" name="like_post_id" value="<?php echo $post['id']; ?>">
+										<input type="image" name="like" src="images/like.png" alt="いいね" width="13px" height="13px">
+										<?php if ($cntLike > 0) : ?>
+											<input type="submit" class="like_form" value="<?php echo h($cntLike); ?>">
+										<?php endif; ?>
+									</form>
+								<?php else : ?>
+									<!--いいね取り消し機能-->
+									<form action="" method="get">
+										<input type="hidden" name="like" value="delete">
+										<input type="hidden" name="like_post_id" value="<?php echo $post['id']; ?>">
+										<input type="image" src="images/after_like.png" alt="いいね取り消し" width="13px" height="13px">
+										<?php if ($cntLike > 0) : ?>
+											<input type="submit" class="like_form on_like_form" value="<?php echo h($cntLike); ?>">
+										<?php endif; ?>
+									</form>
+								<?php endif; ?>
+								<!--以下リツイート機能-->
+								<?php
+								$cntRetweets->execute(array($post['id']));
+								$cntRetweet = $cntRetweets->fetchColumn();
+								if (!binarySearch($retweetArr, $post['id'])) : ?>
+									<!--投稿に対して、リツイートしたことがない場合-->
+									<form action="" method="post">
+										<input type="hidden" name="retweet" value="send">
+										<input type="hidden" name="origin_message" value="<?php echo ($post['message']); ?>">
+										<input type="hidden" name="origin_member_id" value="<?php echo ($post['member_id']); ?>">
+										<input type="hidden" name="origin_id" value="<?php echo ($post['id']); ?>">
+										<input type="image" src="images/retweet.png" alt="リツイートする" width="13px" height="13px">
+										<?php if ($cntRetweet > 0) : ?>
+											<input type="submit" class="like_form" value="<?php echo h($cntRetweet); ?>">
+										<?php endif; ?>
+									</form>
+								<?php else : ?>
+									<!--リツイートしたことがある＝取り消す-->
+									<form action="" method="post">
+										<input type="hidden" name="retweet" value="delete">
+										<input type="hidden" name="origin_id" value="<?php echo h($post['id']); ?>">
+										<input type="image" src="images/after_retweet.png" alt="リツイートを取り消す" width="13px" height="13px">
+										<?php if ($cntRetweet > 0) : ?>
+											<input type="submit" class="like_form on_retweet_form" value="<?php echo h($cntRetweet); ?>">
+										<?php endif; ?>
+									</form>
+								<?php endif; ?>
+							<?php } ?>
 							<?php if ($post['reply_post_id'] > 0) : ?>
 								<a href="view.php?id=<?php echo h($post['reply_post_id']); ?>">返信元のメッセージ</a>
 							<?php endif; ?>
